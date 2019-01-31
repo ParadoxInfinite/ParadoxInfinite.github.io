a8df4082e11dce04150745411146901dd79082a0
paradoxinfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2019-02-01 01:56:14 +0530

    Using animate.css, improved a lot of the UI, add js. Need to work on other pages.

--- js/master.js
+++ js/master.js
@@ -1,7 +1,10 @@
-$("#a,#e,#c").click(function(){
-  $("li").removeClass("active");
-  $(this).toggleClass("active");
-});
-$("h2,img").click(function(){
-  $("li").removeClass("active");
+$('h1').addClass('animated flash slow infinite')
+$('body').keypress(function(e){
+  if(e.keyCode==101||e.keyCode==69){
+    $('h1').addClass('hidden');
+    $('body').addClass('shown bgani');
+    $('#mainpage').removeClass('hidden');
+    $('#ac1').addClass('animated fadeInDown');
+    $('#ac2').addClass('animated fadeInUp');
+  }
 })
