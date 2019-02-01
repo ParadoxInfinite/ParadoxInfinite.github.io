0d785e9a36a52ea40e0f03a666cb2d69ac749cd5
paradoxinfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2019-02-01 12:33:16 +0530

    Moderate js work done. About page removed, education page completed.

--- js/master.js
+++ js/master.js
@@ -1,10 +1,27 @@
-$('h1').addClass('animated flash slow infinite')
+$('h1').addClass('animated flash slow infinite');
 $('body').keypress(function(e){
   if(e.keyCode==101||e.keyCode==69){
-    $('h1').addClass('hidden');
-    $('body').addClass('shown bgani');
+    $('body').addClass('bgani');
+    $('h1').remove();
+    $('#mainpage').addClass('shown');
     $('#mainpage').removeClass('hidden');
     $('#ac1').addClass('animated fadeInDown');
     $('#ac2').addClass('animated fadeInUp');
   }
-})
+});
+
+$(document).keypress(function(e) {
+    var pos = $(window).scrollTop();
+    var speed = 1.5;
+    var two = $('#two').scrollTop();
+    var three = $('#three').scrollTop();
+
+    if (e.keyCode == 70 || e.keyCode == 102) {
+      $('#Ho,#Sk').removeClass('active');
+      $('#Ed').addClass('active');
+      var education = $('#education').position().top;
+      var distance = Math.abs(pos + education);
+      var scrollSpeed = distance * speed;
+      $('html, body').animate({scrollTop : education}, 700);
+    }
+});
