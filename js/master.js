708e59be70f10fbbe11bac8ffeaa4e1a6b4b4572
paradoxinfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2019-02-09 21:42:53 +0530

    Loads of improvements done. Skills page updated, responsiveness checked.

--- js/master.js
+++ js/master.js
@@ -1,4 +1,8 @@
 $('h1').addClass('animated flash slow infinite');
+$('.nav-item').click(function(){
+  $('.nav-item').removeClass('active');
+  $(this).addClass('active');
+});
 $('body').keypress(function(e){
   if(e.keyCode==101||e.keyCode==69){
     $('body').addClass('bgani');
@@ -9,19 +13,28 @@ $('body').keypress(function(e){
     $('#ac2').addClass('animated fadeInUp');
   }
 });
-
 $(document).keypress(function(e) {
     var pos = $(window).scrollTop();
-    var speed = 1.5;
-    var two = $('#two').scrollTop();
-    var three = $('#three').scrollTop();
 
     if (e.keyCode == 70 || e.keyCode == 102) {
-      $('#Ho,#Sk').removeClass('active');
-      $('#Ed').addClass('active');
+      $('#Home,#Ski').removeClass('active');
+      $('#Edu').addClass('active');
       var education = $('#education').position().top;
       var distance = Math.abs(pos + education);
-      var scrollSpeed = distance * speed;
-      $('html, body').animate({scrollTop : education}, 700);
+      if(distance!=0) {$('html, body').animate({scrollTop : education}, 800);}
+    }
+    if (e.keyCode == 81 || e.keyCode == 113) {
+      $('#Home,#Edu').removeClass('active');
+      $('#Ski').addClass('active');
+      var skills = $('#skills').position().top;
+      var distance = Math.abs(pos + skills);
+      if(distance!=0){$('html, body').animate({scrollTop : skills}, 800);}
+    }
+    if (e.keyCode == 82 || e.keyCode == 114) {
+      $('#Edu,#Ski').removeClass('active');
+      $('#Home').addClass('active');
+      var home = $('#mainpage').position().top;
+      var distance = Math.abs(pos + home);
+      if(distance!=0){$('html, body').animate({scrollTop : home}, 800);}
     }
 });
