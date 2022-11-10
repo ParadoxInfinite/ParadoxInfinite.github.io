ae3afc55f9d4c8904ad0f164282d13edb311a91a
ParadoxInfinite <10831629+ParadoxInfinite@users.noreply.github.com>
2022-11-11 03:06:56 +0530

    chore: updated details

--- js/master.js
+++ js/master.js
@@ -1,44 +1,59 @@
-$('h1').addClass('animated flash slow infinite');
-$('.nav-item').click(function(){
-  $('.nav-item').removeClass('active');
-  $(this).addClass('active');
+$("h1").addClass("animated flash slow infinite");
+$(".nav-item").click(function () {
+  $(".nav-item").removeClass("active");
+  $(this).addClass("active");
 });
-$('.navbar-brand').click(function(){
-  $('.nav-item').removeClass('active');
-  $('#Home').addClass('active');
+$(".navbar-brand").click(function () {
+  $(".nav-item").removeClass("active");
+  $("#Home").addClass("active");
 });
-$('body').keypress(function(e){
-  if(e.keyCode==101||e.keyCode==69){
-    $('body').addClass('bgani');
-    $('h1').remove();
-    $('#mainpage').addClass('shown');
-    $('#mainpage').removeClass('hidden');
-    $('#ac1').addClass('animated fadeInDown');
-    $('#ac2').addClass('animated fadeInUp');
+$("body").keypress(function (e) {
+  if (e.keyCode == 101 || e.keyCode == 69) {
+    $("body").addClass("bgani");
+    $("h1").remove();
+    $("#mainpage").addClass("shown");
+    $("#mainpage").removeClass("hidden");
+    $("#ac1").addClass("animated fadeInDown");
+    $("#ac2").addClass("animated fadeInUp");
   }
 });
-$(document).keypress(function(e) {
-    var pos = $(window).scrollTop();
+$(document).keypress(function (e) {
+  var pos = $(window).scrollTop();
 
-    if (e.keyCode == 70 || e.keyCode == 102) {
-      $('#Home,#Ski').removeClass('active');
-      $('#Edu').addClass('active');
-      var education = $('#education').position().top;
-      var distance = Math.abs(pos + education);
-      if(distance!=0) {$('html, body').animate({scrollTop : education}, 800);}
+  if (e.keyCode == 70 || e.keyCode == 102) {
+    $("#Home,#Ski,#Work").removeClass("active");
+    $("#Edu").addClass("active");
+    var education = $("#education").position().top;
+    var distance = Math.abs(pos + education);
+    if (distance != 0) {
+      $("html, body").animate({ scrollTop: education }, 800);
     }
-    if (e.keyCode == 81 || e.keyCode == 113) {
-      $('#Home,#Edu').removeClass('active');
-      $('#Ski').addClass('active');
-      var skills = $('#skills').position().top;
-      var distance = Math.abs(pos + skills);
-      if(distance!=0){$('html, body').animate({scrollTop : skills}, 800);}
+  }
+  if (e.keyCode == 87 || e.keyCode == 119) {
+    $("#Home,#Edu,#Ski").removeClass("active");
+    $("#Work").addClass("active");
+    var skills = $("#work").position().top;
+    var distance = Math.abs(pos + skills);
+    if (distance != 0) {
+      $("html, body").animate({ scrollTop: skills }, 800);
     }
-    if (e.keyCode == 82 || e.keyCode == 114) {
-      $('#Edu,#Ski').removeClass('active');
-      $('#Home').addClass('active');
-      var home = $('#mainpage').position().top;
-      var distance = Math.abs(pos + home);
-      if(distance!=0){$('html, body').animate({scrollTop : home}, 800);}
+  }
+  if (e.keyCode == 81 || e.keyCode == 113) {
+    $("#Home,#Edu,#Work").removeClass("active");
+    $("#Ski").addClass("active");
+    var skills = $("#skills").position().top;
+    var distance = Math.abs(pos + skills);
+    if (distance != 0) {
+      $("html, body").animate({ scrollTop: skills }, 800);
     }
+  }
+  if (e.keyCode == 82 || e.keyCode == 114) {
+    $("#Edu,#Ski,#Work").removeClass("active");
+    $("#Home").addClass("active");
+    var home = $("#mainpage").position().top;
+    var distance = Math.abs(pos + home);
+    if (distance != 0) {
+      $("html, body").animate({ scrollTop: home }, 800);
+    }
+  }
 });
